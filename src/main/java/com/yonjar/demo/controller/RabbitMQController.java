96225d34f8fa125c8a7baf484ef062724a0ce5fd
luoyj <luoyj@authine.com>
2021-01-18 13:39:26 +0800

    新增gitignore2

--- src/main/java/com/yonjar/demo/controller/RabbitMQController.java
+++ src/main/java/com/yonjar/demo/controller/RabbitMQController.java
@@ -5,7 +5,6 @@ import com.alibaba.cola.dto.SingleResponse;
 import com.yonjar.demo.cmd.MessageCmd;
 import com.yonjar.demo.service.HelloSender;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
