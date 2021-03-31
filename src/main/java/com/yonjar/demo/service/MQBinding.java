8cb106da1f34b327453ff7d3a935ec18ad2a997d
yonjarluo <yonjar@163.com>
2021-03-31 17:07:23 +0800

    添加注解

--- src/main/java/com/yonjar/demo/service/MQBinding.java
+++ src/main/java/com/yonjar/demo/service/MQBinding.java
@@ -9,6 +9,9 @@ import org.springframework.messaging.SubscribableChannel;
  * @Author luoyj
  * @Date 2021/3/31.
  * 使用 spring-cloud-stream 集成 RabbitMQ
+ * 非RabbitMQ机器启动本项目，需要 创建另一个账户，并赋予读写权限
+ * guest用户只适用local连接
+ * 账户没有权限报错：An unexpected connection driver error occured (Exception message: Socket closed)
  */
 public interface MQBinding {
 
